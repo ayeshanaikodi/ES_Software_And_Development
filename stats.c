9e4d9b39a38bd44c88fac417b6e1c6d2a1ba9eba
Ayesha Naikodi <ayeshanaikodi9@gmail.com>
2020-05-16 23:14:56 -0500

    Adding some missed empty function definitions

--- stats.c
+++ stats.c
@@ -56,6 +56,26 @@ void print_array(unsigned char *array, unsigned int length)
 
 }
 
+unsigned char find_median(unsigned char *array, unsigned int length)
+{
+
+}
+
+unsigned char find_mean(unsigned char *array, unsigned int length)
+{
+
+}
+
+unsigned char find_maximum(unsigned char *array, unsigned int length)
+{
+
+}
+
+unsigned char find_minimum(unsigned char *array, unsigned int length)
+{
+
+}
+
 
 void sort_array(unsigned char *array, unsigned int length)
 {
